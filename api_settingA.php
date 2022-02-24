ec6e4cebb097341b2c7a1ccc57a2f6368c3f7041
HUANG CHEN WEI <Wei.Huang@NMA.Local>
2022-02-24 14:28:13 +0800

    fix image

--- api_settingA.php
+++ api_settingA.php
@@ -320,7 +320,7 @@ function show_SmallThumbnail_lazy($video_id,$cateid , $catename,$des,$cdate,$thu
     		echo '<img class="lazy" data-original="'.THIS_SITE.TB_TEST.'" width="320" height="180">';
     		else
     	    // echo '<img class="lazy" data-original="http://cms.nextanimation.com.tw/'.$thumb.'" width="320" height="180">';
-			echo '<img class="lazy" data-original="http://cms.nextanimation.com.tw/'.$thumb.'" width="320" height="180">';
+			echo '<img src="http://cms.nextanimation.com.tw/'.$thumb.'" width="320" height="180">';
     }
 
     echo '<div class="movlabel" style="margin-top: -3px;">';
