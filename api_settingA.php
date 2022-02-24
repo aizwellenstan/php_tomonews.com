5813ab0065efa4ee6a83095503e0bc4dcf3ec32a
HUANG CHEN WEI <Wei.Huang@NMA.Local>
2022-02-24 14:25:06 +0800

    fix image

--- api_settingA.php
+++ api_settingA.php
@@ -319,7 +319,7 @@ function show_SmallThumbnail_lazy($video_id,$cateid , $catename,$des,$cdate,$thu
     	    if($NSFW_v)
     		echo '<img class="lazy" data-original="'.THIS_SITE.TB_TEST.'" width="320" height="180">';
     		else
-			echo '<img class="lazy" src="http://cms.nextanimation.com.tw/'.$thumb.'" width="320" height="180">';
+			echo '<img src="http://cms.nextanimation.com.tw/'.$thumb.'" width="320" height="180">';
     	    // echo '<img class="lazy" data-original="'.$thumb.'" width="320" height="180">';
     }
 
